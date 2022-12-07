bf30a6c784e73d79ff7ae82be967060b534b5ede
Shyunwoo219gmail.com <90670046+Shyunwoo@users.noreply.github.com>
2022-12-07 11:27:10 +0900

    create project

---
+++ Source/Slash/Items/Item.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+#include "Slash/DebugMacros.h"
+
+AItem::AItem() 
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UWorld* World=GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward=GetActorForwardVector();
+	
+	DRAW_SPHERE(Location);
+	DRAW_VECTOR(Location, Location+Forward*100.f);
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
